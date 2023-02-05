af4f4b60f5741302eb3ecd6907dd389a16e0a6e7
Samuel Sarette <samuelsarette@linux.com>
2023-02-05 14:05:16 -0500

    made valve final mechanical tweaks

--- game/level_maps/gardens/scripts/valve_and_pipe.gd
+++ game/level_maps/gardens/scripts/valve_and_pipe.gd
@@ -1,28 +1,37 @@
-tool
 extends Spatial
 
 onready var water = $WaterFillPipe/Water
 onready var valve = $ValveAssembly/Valve
+onready var water_flowing_sound = $WaterFlowingSound
+onready var animation_player = $AnimationPlayer
 
-export var water_level : float = 0 setget change_water_level
-export var zero_water_y := -1.12
-export var full_water_y := 1.852
+export var is_full := false
 
 signal water_level_changed(percent)
-
-func _ready():
-	change_water_level(water_level)
 
 func change_water_level(amount: float) -> void:
 	if amount < 0 or amount > 1:
 		push_warning("Cannot make water anything not between 0 and 1")
 		return
-	water_level = amount
+
+	if amount < 1:
+		amount = 0
 
 	if not is_inside_tree():
 		return
 
-	water.translation.y = lerp(zero_water_y, full_water_y, amount)
+	var now_full := amount >= 0.99
+	if is_full == now_full:
+		return
+	is_full = now_full
+
+	if now_full:
+		animation_player.play("OffToFullOn")
+		water_flowing_sound.play()
+	else:
+		animation_player.play("OnToOff")
+		water_flowing_sound.stop()
+
 	emit_signal("water_level_changed", amount)
 
 
