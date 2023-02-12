ba9c539dc65cef9f3bd379a448a09267a5010c18
Samuel Sarette <samuelsarette@linux.com>
2023-02-12 18:08:44 -0500

    delay valve-on growing plants

--- game/level_maps/gardens/parts/garden.gd
+++ game/level_maps/gardens/parts/garden.gd
@@ -3,6 +3,8 @@ extends Spatial
 
 export(int, "none", "left", "right") var valve_side := 0
 
+export(float, 0.0, 10, 3.0) var valve_on_plant_grow_delay : float = 3.0
+
 var water_full := false
 
 func _on_valve_water_full(is_full: bool) -> void:
@@ -10,8 +12,15 @@ func _on_valve_water_full(is_full: bool) -> void:
 
 	# now trigger the make_watered on plants already planted
 	var children := get_children()
+	var plants = []
 	for c in children:
 		if c is XRToolsSnapZone:
 			if is_instance_valid(c.picked_up_object) \
 			and c.picked_up_object.has_method("make_watered"):
-				c.picked_up_object.make_watered()
+				plants.append(c.picked_up_object)
+	call_deferred("_wait_then_water", plants)
+
+func _wait_then_water(plants):
+	yield(get_tree().create_timer(valve_on_plant_grow_delay), "timeout")
+	for plant in plants:
+		plant.make_watered()
