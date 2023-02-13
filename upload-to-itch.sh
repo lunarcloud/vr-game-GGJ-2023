07805e831c9be9b3f389eb0bedeca1e64136ec50
Samuel Sarette <samuelsarette@linux.com>
2023-02-12 21:05:49 -0500

    build includes linux, new screenshot

--- upload-to-itch.sh
+++ upload-to-itch.sh
@@ -2,9 +2,11 @@
 CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 APK="$CURRENT_DIR/build/quest/ExoGardener.apk"
 WINDOWS="$CURRENT_DIR/build/windows/"
+LINUX="$CURRENT_DIR/build/linux/"
 
 echo Uploading to Itch...
 butler -v push "$APK" "samsarette/exo-gardener:android"
 butler -v push "$WINDOWS" "samsarette/exo-gardener:windows"
+butler -v push "$LINUX" "samsarette/exo-gardener:linux"
 
 echo Done.
