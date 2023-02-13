8c640a909777649ab5ecce99c978ce6c58c8bced
Samuel Sarette <samuelsarette@linux.com>
2023-02-12 20:29:41 -0500

    remove unused windows batch upload

--- upload-to-itch.bat
+++
@@ -1,9 +0,0 @@
-SET CURRENT_DIR=%~dp0
-SET APK=%CURRENT_DIR%/build/quest/Ataito.apk
-SET WINDOWS=%CURRENT_DIR%/build/windows/
-
-echo Uploading to Itch...
-butler -v push "%APK%" "samsarette/exo-gardener:android"
-butler -v push "%WINDOWS%" "samsarette/exo-gardener:windows"
-
-echo Done.
